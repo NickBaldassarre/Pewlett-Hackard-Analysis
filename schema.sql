ab80f0901fa0c16f50086f13d9da784bf323f90f
Nick Baldassarre <97616788+NickBaldassarre@users.noreply.github.com>
2022-04-23 06:16:56 -0400

    Added queries.sql and retirement_info.csv

--- schema.sql
+++ schema.sql
@@ -36,13 +36,13 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-  dept_no VARCHAR(4) NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no, dept_no)
+    emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -50,6 +50,5 @@ CREATE TABLE titles (
 	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
